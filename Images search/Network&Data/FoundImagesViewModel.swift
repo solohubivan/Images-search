9038188c87d9a77aaed01b05208fff4d8a678c7f
solohubivan <solohubivan@gmail.com>
2024-03-06 22:51:43 +0200

    resultVC showing result of amount available pictures...

---
+++ Images search/Network&Data/FoundImagesViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FoundImagesViewModel.swift
+//  Images search
+//
+//  Created by Ivan Solohub on 05.03.2024.
+//
+
+import Foundation
+
+struct FoundImagesViewModel: Codable {
+    var total: Int
+    var pageURL: String
+    var type: String
+    var tags: String
+}
---
+++ Images search/Network&Data/PixabayDataMeneger.swift
@@ -0,0 +1,8 @@
+//
+//  PixabayDataMeneger.swift
+//  Images search
+//
+//  Created by Ivan Solohub on 05.03.2024.
+//
+
+import Foundation
